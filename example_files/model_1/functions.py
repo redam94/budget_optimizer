7d44a4a7d234eb2e3dc99e4fe821996930277f2c
Matthew Reda <m.reda94@gmail.com>
2024-11-23 22:07:17 -0500

    Updating basebudgetmodel

---
+++ example_files/model_1/functions.py
@@ -0,0 +1,27 @@
+import xarray as xr
+from pathlib import Path
+import numpy as np
+from budget_optimizer.utils.model_classes import _Model
+
+class SimpleModel(_Model):
+  def __init__(self, data: xr.Dataset = None):
+    self.data = data
+    
+  def predict(self, x: xr.Dataset) -> xr.DataArray:
+    x = x.copy()
+    x["prediction"] = x["a"] + x["b"]
+    return x["prediction"]
+  
+  def contributions(self, x: xr.Dataset) -> xr.Dataset:
+    return x
+
+def budget_to_data(budget, model):
+    data = model.data.copy()
+    for key, value in budget.items():
+        data[key] = value*data[key]
+    return data
+  
+def model_loader(path: Path):
+    data_a = xr.DataArray(np.array([1, 2, 3]), dims='time', coords={"time": np.array([1, 2, 3])})
+    data_b = xr.DataArray(np.array([4, 5, 6]), dims='time', coords={"time": np.array([1, 2, 3])})
+    return SimpleModel(data = xr.Dataset({"a": data_a, "b": data_b}))
